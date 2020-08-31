66a3943b2cdf46a6155c77b99c28c130261f786e
Bruno G Alcantara <inovox@gmail.com>
2020-08-30 22:11:59 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { MovieComponent } from './movie/movie.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    SearchboxComponent,
+    MovieComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule
